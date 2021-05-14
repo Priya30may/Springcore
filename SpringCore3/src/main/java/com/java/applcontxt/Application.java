eb6949d5d1ccdad0d4f060e98d545483cfe2a613
priya <priya@LAPTOP-DJPUL99C>
2021-05-14 18:48:13 +0530

    core

---
+++ SpringCore3/src/main/java/com/java/applcontxt/Application.java
@@ -0,0 +1,10 @@
+package com.java.applcontxt;
+
+public class Application {
+
+	public void go() {
+
+		System.out.println("Priya");
+	}
+
+}
---
+++ SpringCore3/src/main/java/com/java/applcontxt/Applicationclass.java
@@ -0,0 +1,11 @@
+package com.java.applcontxt;
+
+public class Applicationclass {
+
+	public void walk() {
+
+		System.out.println("Dharshini");
+	}
+
+	
+}
